ec13925bef7e290c3a9de4702b3444bafaece3a5
arunabh-lejhro <113446187+arunabh-lejhro@users.noreply.github.com>
2022-10-27 13:31:20 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+app = FastAPI()
+
+class request_body(BaseModel):
+    gender:object
+    married:object
+    depend:object
+    edu:object
+    selfemp:object
+    app:int
+    coapp:float
+    loan:float
+    loanterm:float
+    credit:float
+    prop:object
+
+#Train Model
+df=pd.read_csv(r"loan_data.csv")
+del df['Loan_ID']
+X=df.drop("Loan_Status", axis=1)
+y=df['Loan_Status']
+X.Gender.fillna('Male', inplace=True)
+X.Married.fillna('Yes', inplace=True)
+X.Dependents.fillna('0', inplace=True)
+X.Self_Employed.fillna('No', inplace=True)
+X.LoanAmount.fillna(np.mean(X.LoanAmount), inplace=True)
+X.Loan_Amount_Term.fillna(np.mean(X.Loan_Amount_Term), inplace=True)
+X.Credit_History.fillna(1.0, inplace=True)
+X=pd.get_dummies(X)
+rfc=RandomForestClassifier()
+rfc.fit(X, y)
+
+
+def predict_output(gender, married, depend, edu, selfemp, app, coapp, loan, loanterm, credit,prop):
+    value=[[gender, married, depend,edu, selfemp, app, coapp, loan, loanterm, credit, prop]]
+    newdf=pd.DataFrame(value, columns=['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed',
+       'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount',
+       'Loan_Amount_Term', 'Credit_History', 'Property_Area'])
+    newdf=pd.get_dummies(newdf)
+    missing_cols=set(X.columns)-set(newdf.columns)
+    for i in missing_cols:
+        newdf[i]=0
+    newdf=newdf[X_train.columns]
+    new_prediction=rfc.predict(newdf)
+    if(new_prediction[0]=='Y'):
+        return "Y"
+    else:
+        return "N"
+
+
+@app.post("/api/predict-loan")
+async def predict_loan(data : request_body):   
+    return {
+        "data": predict_output(data.gender, data.married, data.depend, data.edu, data.selfemp, data.app, data.coapp, data.loan, data.loanterm, data.credit, data.prop)
+    }
