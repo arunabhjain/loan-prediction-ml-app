a085fe153e26e1f009038090a5e95f782307a983
arunabh-lejhro <113446187+arunabh-lejhro@users.noreply.github.com>
2022-10-28 09:21:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -69,6 +69,6 @@ def read_root():
     
 @app.post("/api/predict-loan")
 async def predict_loan(data : request_body):   
-    return jsonable_encoder{
+    return {
         "data": predict_output(data.gender, data.married, data.depend, data.edu, data.selfemp, data.app, data.coapp, data.loan, data.loanterm, data.credit, data.prop)
     }
