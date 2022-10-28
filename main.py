679f7a011a4a56bd0ef774e4f3271c82bd967b19
arunabh-lejhro <113446187+arunabh-lejhro@users.noreply.github.com>
2022-10-28 09:28:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["*"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
